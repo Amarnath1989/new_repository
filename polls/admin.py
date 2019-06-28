df70a323e10092a50e09f00db040cd13c3be1ddd
Amarnath <pamarnath1989@gmail.com>
2019-06-28 11:37:33 +0530

    adding mysite2

---
+++ polls/admin.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from polls.models import Question,Choice
+
+# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question_text','pub_date']
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['choice_text','votes']
+
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice,ChoiceAdmin)
